10306d6ef871f5430c6fc6bb3401ba1e75a7132d
Abbey Myrick <abm0044@uah.edu>
2026-04-24 20:47:26 -0500

    Update version6.cpp
    + comments

--- version6.cpp
+++ version6.cpp
@@ -10,41 +10,41 @@ Date: 4-16-2026
 using namespace std;
 
 int ask_question(string question, string options[], char correct_answer) {
-    cout << question << endl;
+    cout << question << endl;// prints out the questions 
 
     for (int i = 0; i < 4; i++) {
-        cout << options[i] << endl;
+        cout << options[i] << endl;// prints out all answer options 
     }
 
     char user_answer;
-    cout << "Your answer: ";
+    cout << "type selected answer here: ";
     cin >> user_answer;
 
-    if (toupper(user_answer) == correct_answer) {  //Converts user input to uppercase
-        cout << "Correct!\n\n";
+    if (toupper(user_answer) == correct_answer) {  //Ensures all answers are in uppercase to match the file
+        cout << "Correct! one point earned\n\n";
         return 1;
     } else {
-        cout << "Wrong! Correct answer was " << correct_answer << "\n\n";
+        cout << "Wrong! Correct answer was:" << correct_answer <<"no points earned<< "\n\n";
         return 0;
     }
 }
 
 int main() {
     int score = 0;
-    ifstream quiz_file("mathQuizupdated.txt");
+    ifstream quiz_file("mathQuizupdated.txt");// file format includes 10 sets of questions starting with one life four the question, 4 four the answer choices one for the correct answer and a space for readability.
     string question;
     string choices[4];
     char answer;
     
-    while(getline(quiz_file, question)){
+    while(getline(quiz_file, question)){// reads in the question first 
         for (int i = 0; i < 4; i++){
-            getline(quiz_file, choices[i]);
+            getline(quiz_file, choices[i]);// then the four answer choices 
         }
         
-        quiz_file >> answer;
-        quiz_file.ignore();
+        quiz_file >> answer; // and finally the answer
+        quiz_file.ignore();// skips the blank line.
         
-        score += ask_question(question, choices, answer);
+        score += ask_question(question, choices, answer);// tracks score throught the quiz starting from 0/10
         
     }
 
@@ -53,3 +53,4 @@ int main() {
     quiz_file.close();
     return 0;
 }
+// Our code is now able to read questions from a file which is the biggest upgrade since the last milestone of this project.
